354def777b06925f5a28d3f6da0cc90eccaa832b
1ly4bd <abdulogunleye76@gmail.com>
2026-09-13 23:48:59 +0200

    feat: enhance mobile card functionality and improve layout responsiveness

--- frontend/app.js
+++ frontend/app.js
@@ -395,7 +395,9 @@ function connectWebSocket() {
 function updateHUDCard(data) {
   if (!data) return;
   
-  card.classList.remove('hidden');
+  if (window.innerWidth > 800) {
+    card.classList.remove('hidden');
+  }
   card.classList.add('mobile-open')
 
   // formating date
@@ -528,4 +530,21 @@ cardTab.addEventListener('click', () => {
 
 cardClose.addEventListener('click', () => {
   card.classList.remove('mobile-open');
+});
+
+window.addEventListener('pointerdown', (event) => {
+  if (window.innerWidth <= 800) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(livePostsData);
+    
+    if (intersects.length > 0) {
+      card.classList.remove('hidden');
+      updateHUDCard(intersects[0].object.userData);
+    } else {
+      card.classList.add('hidden');
+    }
+  }
 });
